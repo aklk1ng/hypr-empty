0c3a239f3ac700cd406731cfeb84b2265a65b73a
aklk1ng <2628084745@qq.com>
2023-11-05 20:48:36 +0800

    feat: repeat try to start the listener

--- README.md
+++ README.md
@@ -13,17 +13,29 @@ cargo install --git=https://github.com/nate-sys/hypr-empty
 
 ## Usage
 1) Create the file `~/.config/hypr-empty/config.toml`
-2) Specify the command you want to be run 
+2) Specify the command you want to be run and the `unique` workspace
 ```toml
 # If you're using wofi
+[[components]]
+workspace = "2"
 command = "wofi"
 args = ["-S", "drun"]
 
 # If you're using rofi
+[[components]]
+workspace = "3"
 command = "rofi"
 args = ["-show", "drun"]
 
+# If you want open the browser when you arrive the workspace
+[[components]]
+workspace = "13"
+command = "google-chrome-stable"
+args = []
+
 # arbitrary program
+[[components]]
+workspace = "NUMBER"
 command = "name_of_program"
 args = ["arg1", "arg2"]
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use hyprland::shared::WorkspaceType;
 use serde::Deserialize;
+use std::thread;
+use std::time::Duration;
 use std::{fs::File, io::Read, process::Command};
 
 #[derive(Deserialize)]
@@ -20,33 +22,45 @@ use hyprland::{
 };
 
 fn main() -> Result<()> {
-    let mut event_listener = EventListener::new();
+    loop {
+        let mut event_listener = EventListener::new();
 
-    let home_dir = std::env::var("HOME")?;
-    let mut config_file = File::open(format!("{home_dir}/.config/hypr-empty/config.toml"))?;
+        let home_dir = std::env::var("HOME")?;
+        let mut config_file = File::open(format!("{home_dir}/.config/hypr-empty/config.toml"))?;
 
-    let mut config = String::new();
-    config_file.read_to_string(&mut config)?;
+        let mut config = String::new();
+        config_file.read_to_string(&mut config)?;
 
-    let cmds: Cmds = toml::from_str(&config)?;
+        let cmds: Cmds = toml::from_str(&config)?;
 
-    event_listener.add_workspace_change_handler(move |id, state| {
-        if let WorkspaceType::Regular(_ws) = &state.active_workspace {
-            let mut cmds = cmds
-                .components
-                .iter()
-                .filter(|&cmd| *cmd.workspace == id.to_string());
-            if let Some(cmd) = &cmds.next() {
-                if Workspace::get_active().unwrap().windows == 0 {
-                    Command::new(&cmd.command)
-                        .args(cmd.args.clone().as_deref().unwrap_or_default())
-                        .spawn()
-                        .unwrap();
+        event_listener.add_workspace_change_handler(move |id, state| {
+            if let WorkspaceType::Regular(_ws) = &state.active_workspace {
+                let mut cmds = cmds
+                    .components
+                    .iter()
+                    .filter(|&cmd| *cmd.workspace == id.to_string());
+                if let Some(cmd) = &cmds.next() {
+                    if Workspace::get_active().unwrap().windows == 0 {
+                        Command::new(&cmd.command)
+                            .args(cmd.args.clone().as_deref().unwrap_or_default())
+                            .spawn()
+                            .unwrap();
+                    }
                 }
             }
-        }
-    });
+        });
 
-    event_listener.start_listener()?;
+        let mut cnt = 0;
+        match event_listener.start_listener() {
+            Err(_) => {
+                cnt += 1;
+                thread::sleep(Duration::from_secs(1));
+            }
+            Ok(_) => break,
+        }
+        if cnt >= 9 {
+            break;
+        }
+    }
     Ok(())
 }
